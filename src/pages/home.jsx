6e84bb3467c29f81644310468fca9aa68e7683a1
Michael Williams <mwilliams2964@gmail.com>
2020-03-25 01:12:53 -0700

    added link on homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, } from 'react-bootstrap';
 import LinkedInPhoto from '../assets/LinkedInPhoto.jpeg';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
     render() {
@@ -33,12 +34,12 @@ class Home extends Component {
                                                     <a href="https://github.com/mawil583" rel="noopener noreferrer" target="_blank">
                                                         <i className="fa fa-github-square fa-fw" aria-hidden="true" />
                                                     </a>
-                                                
+
                                                     <a href="https://drive.google.com/open?id=1whG6WH0AgOyOM79xBv_nvpCgrFtFx-ic" rel="noopener noreferrer" target="_blank">
                                                         <i className="fa fa-external-link-square fa-fw" aria-hidden="true" />
                                                     </a>
                                                 </Col>
-                                                
+
 
                                             </div>
                                         </Row>
@@ -64,17 +65,17 @@ class Home extends Component {
                                     <h2 id="centerMe">About Me</h2>
                                 </div>
                                 <p>Hello! My name is Michael and I am a chemist turned full-stack software engineer. I have always been drawn
-                                    towards technical areas of study. As far back as I can remember, I've always 
+                                    towards technical areas of study. As far back as I can remember, I've always
                                     excelled in math and science. During my undergrad, philosophy of logic was one of my favorite classes. And
                                     now I found a discipline where I get to work with logic daily. It's a dream come true!
-                                </p> 
-                                <p>Skills of mine include React, React-Bootstrap, JavaScript (ES6), Mongo/Mongoose, SQL/Sequelize, Node.js/Express, RESTful APIs, 
+                                </p>
+                                <p>Skills of mine include React, React-Bootstrap, JavaScript (ES6), Mongo/Mongoose, SQL/Sequelize, Node.js/Express, RESTful APIs,
                                     Command Terminal, Bootstrap, jQuery, HTML5, and CSS3.</p>
                                 <p>This website was created from scratch using JavaScript, Node.js, Express, React, React-Bootstrap, Font-Awesome,
                                     Google Fonts and custom CSS. Because of Bootstrap's grid system, this site is fully responsive. Try it out!
                                     Resize your browser and the content will remain viewable.</p>
 
-                                <p>In my portfolio section you'll be able to see what my skills are able to accomplish. Most of my skills are
+                                <p>In my <Link to='/portfolio'>portfolio</Link> section you'll be able to see what my skills are able to accomplish. Most of my skills are
                                     showcased there. Feel free to contact me for any inquiries.</p>
 
                             </Col>
